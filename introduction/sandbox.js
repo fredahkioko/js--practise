67ba68f515ad79b62b3dbe1e92e78874f14e9850
fredahkioko <fredah.kioko@strathmore.edu>
2020-09-23 23:47:41 +0300

    variables and functions

---
+++ introduction/sandbox.js
@@ -0,0 +1,45 @@
+// //alert('hello,beautiful');
+
+// //log into the console in browsers
+// //console.log(1);
+// //console.log('blue');
+
+
+// //variables:)
+// let myAge=25;
+// let $year=2025;
+// console.log(myAge,$year);
+// //updating an already existing variable
+// age =30;
+// //fixed variables for mordern js
+// const points=9.12;
+// console.log(points);
+ 
+// var score=75;
+// console.log(score);
+
+// //sstring concatenation
+//let email ='mario@gmail.com';
+// let firstname='Fredah';
+// let lastname='kioko';
+// let fullname=firstname+' '+lastname;
+// console.log(fullname);
+
+// // getting characters
+// console.log(fullname[0]);
+// //methods and properties
+// console.log(fullname.length);
+// //function is a method
+// console.log(fullname.toLocaleUpperCase());
+// // let result= fullname.toLowerCase();
+// // console.log(result);
+// //  let index =email.indexOf('@');
+// //  console.log(index);
+// // let win =email.lastIndexOf('g');
+// // console.log(win);
+//the first argument is from and the last is to
+// let rice=email.slice(0,10);
+// console.log(rice);
+// //let rice=email.substr(4,10);
+// let rice=email.replace('m','w');
+// console.log(rice); 
